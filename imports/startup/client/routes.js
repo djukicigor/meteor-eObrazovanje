63314a0dc4218ba00e7e45a376b56c4c675c8ecb
Igor Djukic <igordjukic0@gmail.com>
2018-09-16 18:46:41 +0200

    added login and register

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,20 +1,35 @@
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+import { Meteor } from 'meteor/meteor';
 
 // Import needed templates
 import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
 import '../../ui/pages/not-found/not-found.js';
+import '../../ui/pages/login/login.js';
 
 // Set up all routes in the app
 FlowRouter.route('/', {
   name: 'App.home',
+  triggersEnter: [(context, redirect) => {
+    if (Meteor.userId) {
+      redirect('App.login');
+    }
+  }],
   action() {
     this.render('App_body', { main: 'App_home' });
   },
 });
 
 FlowRouter.route('*', {
+  name: 'App.notFound',
   action() {
     this.render('App_body', { main: 'App_notFound' });
   },
 });
+
+FlowRouter.route('/login', {
+  name: 'App.login',
+  action() {
+    this.render('App_login')
+  }
+})
