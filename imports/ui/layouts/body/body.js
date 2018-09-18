3830faf3f1553144d9272a7f65312938009fcf9f
Igor Djukic <igordjukic0@gmail.com>
2018-09-18 22:14:12 +0200

    added body UI

--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -1,1 +1,17 @@
+import { Template } from 'meteor/templating';
+import { _ } from 'meteor/underscore';
+import { AccountsTemplates } from 'meteor/useraccounts:core';
+
 import './body.html';
+
+Template.App_body.events({
+    'click #js-item': function(event, template) {
+        const el = document.getElementsByClassName("menu-item active")[0];
+        el.classList.remove('active');
+        event.currentTarget.classList.add('active');
+    },
+    'click #js-logout': function(event, template) {
+        event.preventDefault();
+        AccountsTemplates.logout();
+    }
+});
