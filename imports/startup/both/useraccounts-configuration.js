081ea13abdcd6508951c1e3f2fadfa188a503382
Igor Djukic <igordjukic0@gmail.com>
2018-09-18 17:16:11 +0200

    fixed logout redirect

--- imports/startup/both/useraccounts-configuration.js
+++ imports/startup/both/useraccounts-configuration.js
@@ -1,3 +1,4 @@
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import { AccountsTemplates } from 'meteor/useraccounts:core';
 
 AccountsTemplates.configureRoute('signIn', {
@@ -5,4 +6,10 @@ AccountsTemplates.configureRoute('signIn', {
     path: '/login',
     template: 'App_login',
     redirect: '/'
-  });
+});
+
+AccountsTemplates.configure({
+    onLogoutHook: () => {
+      FlowRouter.go('/login');
+    }
+});
