f3257ddc857687d148eb50aecca4314febac5a2c
Igor Djukic <igordjukic0@gmail.com>
2018-09-24 16:45:23 +0200

    finished edit subject

--- imports/ui/pages/subjects/subjectEdit.js
+++ imports/ui/pages/subjects/subjectEdit.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { $ } from 'meteor/jquery';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
 import './subjectEdit.html';
 
@@ -16,10 +17,15 @@ Template.App_subjectEdit.onRendered(function () {
 Template.App_subjectEdit.events({
     'submit #js-editSubject': function (event, template) {
         event.preventDefault();
+        const _id = template.data.subject._id;
         Meteor.call('editSubject', {
-            _id: template.data.subject._id,
+            _id,
             title: event.currentTarget[0].value,
             description: event.currentTarget[1].value
+        }, (err, data) => {
+            if (data) {
+                FlowRouter.go('App.subject', { _id })
+            }
         })
     }
 });
