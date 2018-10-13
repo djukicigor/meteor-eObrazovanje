13c2d762175876c854cfb0b69de4ac19cc76b18a
Igor Djukic <igordjukic0@gmail.com>
2018-10-13 21:07:04 +0200

    fixed subjects

--- imports/ui/pages/lectures/lectures.js
+++ imports/ui/pages/lectures/lectures.js
@@ -6,10 +6,14 @@ Template.App_lectures.events({
         event.preventDefault();
         const date = new Date(event.currentTarget[0].value + "T" + event.currentTarget[1].value);
         const subjectId = event.currentTarget.getAttribute('subjectId');
+        event.currentTarget[2].classList.add('loading')
         Meteor.call('makeExam', { subjectId, date }, (err, data) => {
+            event.currentTarget[2].classList.remove('loading', 'primary');
             if (err) {
+                event.currentTarget[2].classList.add('negative');
                 throw new Meteor.Error(400, err);
             }
+            event.currentTarget[2].classList.add('positive');
             event.currentTarget.reset()
         })
     }
